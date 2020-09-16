038f24c0529b04ad17a9cb164f29b6878e87bf67
BananaBeard <kostikololo@gmail.com>
2020-09-16 16:33:25 +0300

    [*] Update Gradle tools version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
     dependencies {
         val kotlinVersion: String by project.extra
         val navigationVersion: String by project.extra
-        classpath("com.android.tools.build:gradle:4.1.0-rc01")
+        classpath("com.android.tools.build:gradle:4.1.0-rc03")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navigationVersion")
     }
