c249dfef812ccec697f8f66e4a2f9feabc77894c
BananaBeard <kostikololo@gmail.com>
2020-09-17 16:15:34 +0300

    [*] Move version management bo buildSrc, update versions

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,14 @@
+object Versions {
+    const val kotlinVersion = "1.4.0"
+    const val navigationVersion = "2.3.0"
+    const val androidXVersion = "1.5.0-alpha02"
+    const val appcompatVersion = "1.2.0"
+    const val materialVersion = "1.3.0-alpha02"
+    const val coroutinesVersion = "1.3.9"
+    const val archLifecycleVersion = "2.2.0"
+    const val retrofitVersion = "2.9.0"
+    const val okhttpVersion = "3.12.3"
+    const val koinVersion = "2.1.5"
+    const val roomVersion = "2.2.5"
+    const val constraintVersion = "2.0.1"
+}
