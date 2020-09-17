b12cbe04b996768e3da93211c38c558594a1233c
BananaBeard <kostikololo@gmail.com>
2020-09-17 16:37:42 +0300

    [*] Update MainActivity with system UI extensions

--- app/src/main/java/com/kovalenko/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/kovalenko/weatherforecast/MainActivity.kt
@@ -2,32 +2,54 @@ package com.kovalenko.weatherforecast
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.shape.CornerFamily
-import com.google.android.material.shape.MaterialShapeDrawable
+import com.kovalenko.weatherforecast.view.clearLightStatusBar
+import com.kovalenko.weatherforecast.view.isDarkModeEnabled
+import com.kovalenko.weatherforecast.view.setLightStatusBar
+import com.kovalenko.weatherforecast.view.setupFullscreen
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupFullscreen(main_root)
         setupNavigation()
     }
 
+    override fun onResume() {
+        super.onResume()
+
+        if (isDarkModeEnabled()) {
+            clearLightStatusBar(main_root)
+        } else {
+            setLightStatusBar(main_root)
+        }
+    }
+
     private fun setupNavigation() {
-        val navController = findNavController(R.id.nav_host)
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        val navController = navHost.navController
         navigation_main.setupWithNavController(navController)
         navigation_main.setOnNavigationItemReselectedListener {
-
         }
 
-        val radius = resources.getDimension(R.dimen.margin_default)
-        val backGround = navigation_main.background as MaterialShapeDrawable
-        backGround.shapeAppearanceModel = backGround.shapeAppearanceModel
-                .toBuilder()
-                .setTopRightCorner(CornerFamily.ROUNDED, radius)
-                .setTopLeftCorner(CornerFamily.ROUNDED, radius)
-                .build()
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            val darkMode = isDarkModeEnabled()
+
+            when (destination.id) {
+                R.id.placePickerFragment -> {
+                    setLightStatusBar(main_root)
+                }
+                else -> {
+                    if (darkMode) {
+                        clearLightStatusBar(main_root)
+                    } else {
+                        setLightStatusBar(main_root)
+                    }
+                }
+            }
+        }
     }
 }
