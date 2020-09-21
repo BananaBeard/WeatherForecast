8b500fa9dac46ef28a67bc19acb7c12dd417a324
BananaBeard <kostikololo@gmail.com>
2020-09-21 14:43:57 +0300

    [*] Update app navigation to multiple backstacks workaround

--- app/src/main/java/com/kovalenko/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/kovalenko/weatherforecast/MainActivity.kt
@@ -1,54 +1,63 @@
 package com.kovalenko.weatherforecast
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
+import androidx.lifecycle.LiveData
+import androidx.navigation.NavController
 import com.kovalenko.weatherforecast.view.clearLightStatusBar
 import com.kovalenko.weatherforecast.view.isDarkModeEnabled
 import com.kovalenko.weatherforecast.view.setLightStatusBar
 import com.kovalenko.weatherforecast.view.setupFullscreen
+import com.kovalenko.weatherforecast.view.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var currentNavController: LiveData<NavController>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupFullscreen(main_root)
-        setupNavigation()
+        setupBottomNavigation()
     }
 
-    override fun onResume() {
-        super.onResume()
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        setupBottomNavigation()
+    }
 
-        if (isDarkModeEnabled()) {
-            clearLightStatusBar(main_root)
-        } else {
+    override fun onRestoreInstanceState(
+        savedInstanceState: Bundle?,
+        persistentState: PersistableBundle?
+    ) {
+        super.onRestoreInstanceState(savedInstanceState, persistentState)
+        setupBottomNavigation()
+    }
+
+    private fun setupBottomNavigation() {
+        val liveData = navigation_main.setupWithNavController(
+            navGraphIds = listOf(R.navigation.graph_forecast, R.navigation.graph_picker),
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host,
+            intent = intent
+        )
+        currentNavController = liveData
+
+
+        currentNavController!!.observe(this, { controller ->
+            setUiMode(controller.currentDestination?.id)
+        })
+    }
+
+    private fun setUiMode(id: Int?) {
+        if (id == R.id.placePickerFragment) {
             setLightStatusBar(main_root)
-        }
-    }
-
-    private fun setupNavigation() {
-        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
-        val navController = navHost.navController
-        navigation_main.setupWithNavController(navController)
-        navigation_main.setOnNavigationItemReselectedListener {
-        }
-
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            val darkMode = isDarkModeEnabled()
-
-            when (destination.id) {
-                R.id.placePickerFragment -> {
-                    setLightStatusBar(main_root)
-                }
-                else -> {
-                    if (darkMode) {
-                        clearLightStatusBar(main_root)
-                    } else {
-                        setLightStatusBar(main_root)
-                    }
-                }
+        } else {
+            if (isDarkModeEnabled()) {
+                clearLightStatusBar(main_root)
+            } else {
+                setLightStatusBar(main_root)
             }
         }
     }
